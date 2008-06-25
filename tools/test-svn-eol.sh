fddba8e3288a4b5fbb884d1dd76edf10b5e3e82a
artojon <artojon@1916b7bc-2d4d-0410-bf2b-f14ee8b928b2>
2008-06-25 11:22:21 +0000

    Added new tool 'test-svn-eol.sh'.
    The tool is a shell script for searching files that don't have the
    "svn:eol-style" property set. This property is important for ensuring
    consistent EOLs in all source code files.
    
    Source code files (.cpp, h) should have LF EOLs *only*. Files that
    are targetted for Windows (non-source code files but text in them)
    should have CRLF EOLs.
    
    
    git-svn-id: https://sloppygui.svn.sourceforge.net/svnroot/sloppygui/trunk@22 1916b7bc-2d4d-0410-bf2b-f14ee8b928b2

---
+++ tools/test-svn-eol.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Prints files that don't have the "svn:eol-style" property set.
+#
+# This shell script will search for files ending with ".cpp", ".h" and ".pro"
+# in the current directory and all sub directories. If the found file doesn't
+# have the "svn:eol-style" property set, its file file name is printed to
+# standard output.
+#
+# Hidden files / directories are exluded from the search.
+#
+# No command line switches are available
+
+for file in `find . \( ! -regex '.*/\..*' -a -name "*.cpp" -o -name "*.h" -o -name "*.pro"  \) -type f`
+do
+	if [ "x`svn propget svn:eol-style`" = "x" ]
+	then	
+		echo "$file"
+	fi
+done
+
+exit 0
