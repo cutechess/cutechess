77fc94893fc431ec23388ff9d9defd4dc0b261ba
Arto Jonsson <ajonsson@kapsi.fi>
2008-08-06 21:56:28 +0300

    Add GPL headers.

--- src/engineconfiguration.cpp
+++ src/engineconfiguration.cpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of SloppyGUI.
+
+    SloppyGUI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SloppyGUI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SloppyGUI.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "engineconfiguration.h"
 
 EngineConfiguration::EngineConfiguration()
