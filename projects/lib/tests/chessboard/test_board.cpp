10c1c9a2619162ee72feae05f39559a1c9581b1e
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2009-02-10 12:46:04 +0200

    Fix the unit tests for Chess::Board

--- projects/lib/tests/chessboard/test_board.cpp
+++ projects/lib/tests/chessboard/test_board.cpp
@@ -66,41 +66,43 @@ void TestBoard::zobristKeys_data() const
 	QTest::addColumn<int>("variant");
 	QTest::addColumn<QString>("fen");
 	QTest::addColumn<quint64>("key");
+	
+	int variant = Chess::Variant::Standard;
 
 	QTest::newRow("startpos")
-		<< (int)Chess::StandardChess
-		<< Chess::standardFen
+		<< variant
+		<< "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 		<< Q_UINT64_C(0x463b96181691fc9c);
 	QTest::newRow("e2e4")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3"
 		<< Q_UINT64_C(0x823c9b50fd114196);
 	QTest::newRow("e2e4 d7d5")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6"
 		<< Q_UINT64_C(0x0756b94461c50fb0);
 	QTest::newRow("e2e4 d7d5 e4e5")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq -"
 		<< Q_UINT64_C(0x662fafb965db29d4);
 	QTest::newRow("e2e4 d7d5 e4e5 f7f5")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6"
 		<< Q_UINT64_C(0x22a48b5a8e47ff78);
 	QTest::newRow("e2e4 d7d5 e4e5 f7f5 e1e2")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR b kq -"
 		<< Q_UINT64_C(0x652a607ca3f242c1);
 	QTest::newRow("e2e4 d7d5 e4e5 f7f5 e1e2 e8f7")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbq1bnr/ppp1pkpp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR w - -"
 		<< Q_UINT64_C(0x00fdd303c946bdd9);
 	QTest::newRow("a2a4 b7b5 h2h4 b5b4 c2c4")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/p1pppppp/8/8/PpP4P/8/1P1PPPP1/RNBQKBNR b KQkq c3"
 		<< Q_UINT64_C(0x3c8123ea7b067637);
 	QTest::newRow("a2a4 b7b5 h2h4 b5b4 c2c4 b4c3 a1a3")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "rnbqkbnr/p1pppppp/8/8/P6P/R1p5/1P1PPPP1/1NBQKBNR b Kkq -"
 		<< Q_UINT64_C(0x5c3f9b829b279560);
 }
@@ -111,7 +113,7 @@ void TestBoard::zobristKeys()
 	QFETCH(QString, fen);
 	QFETCH(quint64, key);
 
-	m_board.setVariant((Chess::Variant)variant);
+	m_board.setVariant((Chess::Variant::Code)variant);
 	QVERIFY(m_board.setBoard(fen));
 	QCOMPARE(m_board.key(), key);
 }
@@ -123,18 +125,20 @@ void TestBoard::moveStrings_data() const
 	QTest::addColumn<QString>("startfen");
 	QTest::addColumn<QString>("endfen");
 	
+	int variant = Chess::Variant::Standard;
+	
 	QTest::newRow("san")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "e4 Nc6 e5 d5 exd6 Be6 Nf3 Qd7 Bb5 O-O-O dxc7 a6 O-O Qxd2 "
 		   "cxd8=N Qxc1 Bxc6 Qxd1 Nxb7 Qxf1+ Kxf1 Bxa2 Rxa2 Kc7"
-		<< Chess::standardFen
+		<< "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 		<< "5bnr/1Nk1pppp/p1B5/8/8/5N2/RPP2PPP/1N3K2 w - - 1 13";
 	QTest::newRow("coord")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "e2e4 b8c6 e4e5 d7d5 e5d6 c8e6 g1f3 d8d7 f1b5 e8c8 d6c7 "
 		   "a7a6 e1g1 d7d2 c7d8n d2c1 b5c6 c1d1 d8b7 d1f1 g1f1 e6a2 "
 		   "a1a2 c8c7"
-		<< Chess::standardFen
+		<< "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 		<< "5bnr/1Nk1pppp/p1B5/8/8/5N2/RPP2PPP/1N3K2 w - - 1 13";
 }
 
@@ -145,7 +149,7 @@ void TestBoard::moveStrings()
 	QFETCH(QString, startfen);
 	QFETCH(QString, endfen);
 
-	m_board.setVariant((Chess::Variant)variant);
+	m_board.setVariant((Chess::Variant::Code)variant);
 	QVERIFY(m_board.setBoard(startfen));
 
 	foreach (const QString& moveStr, moves.split(' '))
@@ -166,39 +170,42 @@ void TestBoard::perft_data() const
 	QTest::addColumn<int>("depth");
 	QTest::addColumn<quint64>("nodecount");
 	
+	int variant = Chess::Variant::Standard;
+	
 	QTest::newRow("startpos")
-		<< (int)Chess::StandardChess
-		<< Chess::standardFen
+		<< variant
+		<< "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 		<< 5
 		<< Q_UINT64_C(4865609);
 	QTest::newRow("pos2")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"
 		<< 4
 		<< Q_UINT64_C(4085603);
 	QTest::newRow("pos3")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "8/3K4/2p5/p2b2r1/5k2/8/8/1q6 b - -"
 		<< 2
 		<< Q_UINT64_C(279);
 	QTest::newRow("pos4")
-		<< (int)Chess::StandardChess
+		<< variant
 		<< "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"
 		<< 6
 		<< Q_UINT64_C(11030083);
 	
+	variant = Chess::Variant::Gothic;
 	QTest::newRow("gothic startpos")
-		<< (int)Chess::CapablancaChess
-		<< Chess::gothicFen
+		<< variant
+		<< "rnbqckabnr/pppppppppp/10/10/10/10/PPPPPPPPPP/RNBQCKABNR w KQkq - 0 1"
 		<< 4
 		<< Q_UINT64_C(808984);
 	QTest::newRow("goth2")
-		<< (int)Chess::CapablancaChess
+		<< variant
 		<< "r1b1c2rk1/p4a1ppp/1ppq2pn2/3p1p4/3A1Pn3/1PN3PN2/P1PQP1BPPP/3RC2RK1 w - -"
 		<< 4
 		<< Q_UINT64_C(7917813);
 	QTest::newRow("goth3")
-		<< (int)Chess::CapablancaChess
+		<< variant
 		<< "r1b2k2nr/p1ppq1ppbp/n1Pcpa2p1/5p4/5P4/1p1PBCPN2/PP1QP1BPPP/RN3KA2R w KQkq -"
 		<< 4
 		<< Q_UINT64_C(4869569);
@@ -211,7 +218,7 @@ void TestBoard::perft()
 	QFETCH(int, depth);
 	QFETCH(quint64, nodecount);
 
-	m_board.setVariant((Chess::Variant)variant);
+	m_board.setVariant((Chess::Variant::Code)variant);
 	QVERIFY(m_board.setBoard(fen));
 	QCOMPARE(smpPerft(m_board, depth), nodecount);
 }
