242870eece1c0a02651e1620c681620e54a77808
Arto Jonsson <ajonsson@kapsi.fi>
2009-05-25 18:12:44 +0300

    Document EngineManager class.

--- projects/lib/src/enginemanager.h
+++ projects/lib/src/enginemanager.h
@@ -20,25 +20,47 @@
 
 #include "engineconfiguration.h"
 
+/*!
+ * \brief Manages chess engines and their configurations.
+ *
+ * \sa EngineConfiguration
+ */
 class LIB_EXPORT EngineManager : public QObject
 {
 	Q_OBJECT
 
 	public:
+		/*! Creates a new EngineManager. */
 		EngineManager(QObject* parent);
 		~EngineManager();
 
+		/*! Adds \a engine to the list of available engines. */
 		void addEngine(const EngineConfiguration& engine);
+		/*! Updates the engine at \a index with \a engine. */
 		void updateEngineAt(int index, const EngineConfiguration& engine);
+		/*! Removes the engine at \a index. */
 		void removeEngineAt(int index);
 
+		/*! Returns the available engines. */
 		QList<EngineConfiguration> engines() const;
+		/*! Sets the available engines to \a engines. */
 		void setEngines(const QList<EngineConfiguration>& engines);
 
 	signals:
+		/*!
+		 * Emitted when all previously queried engine information is now
+		 * invalid and has to be queried again.
+		 */
 		void enginesReset();
+		/*! Emitted when an engine is added to \a index. */
 		void engineAdded(int index);
+		/*!
+		 * Emitted when an engine is removed from \a index.
+		 * \note The engine is already removed when this signal is emitted so
+		 * the index position is no longer valid.
+		 */
 		void engineRemoved(int index);
+		/*! Emitted when an engine is updated at \a index. */
 		void engineUpdated(int index);
 
 	private:
