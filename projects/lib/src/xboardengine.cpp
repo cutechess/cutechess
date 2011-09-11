971df091fccf8334aefba7b4a98cf3881f9978e4
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2011-09-11 14:56:49 +0300

    Xboard option bugfix
    The default values were missing from the EngineSpinOption constructor
    parameter list.

--- projects/lib/src/xboardengine.cpp
+++ projects/lib/src/xboardengine.cpp
@@ -364,12 +364,12 @@ void XboardEngine::setFeature(const QString& name, const QString& val)
 	else if (name == "memory")
 	{
 		if (val == "1")
-			m_options.append(new EngineSpinOption("memory", 32, 0, INT_MAX - 1));
+			m_options.append(new EngineSpinOption("memory", 32, 32, 0, INT_MAX - 1));
 	}
 	else if (name == "smp")
 	{
 		if (val == "1")
-			m_options.append(new EngineSpinOption("cores", 1, 0, INT_MAX - 1));
+			m_options.append(new EngineSpinOption("cores", 1, 1, 0, INT_MAX - 1));
 	}
 	else if (name == "egt")
 	{
