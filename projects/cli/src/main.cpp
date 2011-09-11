3b563ed419b16b6fa4ddbaf802cb26f586a58e92
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2011-09-11 16:23:29 +0300

    Update one more copyright year

--- projects/cli/src/main.cpp
+++ projects/cli/src/main.cpp
@@ -359,7 +359,7 @@ int main(int argc, char* argv[])
 		{
 			out << "cutechess-cli " << CUTECHESS_CLI_VERSION << endl;
 			out << "Using Qt version " << qVersion() << endl << endl;
-			out << "Copyright (C) 2008-2010 Ilari Pihlajisto and Arto Jonsson" << endl;
+			out << "Copyright (C) 2008-2011 Ilari Pihlajisto and Arto Jonsson" << endl;
 			out << "This is free software; see the source for copying ";
 			out << "conditions.  There is NO" << endl << "warranty; not even for ";
 			out << "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.";
