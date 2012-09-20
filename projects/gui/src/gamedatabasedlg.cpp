bb0292c5d6588b5a3ea22f9fdde5b9e2b34b1041
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2012-09-20 12:30:02 +0300

    Use ThreadedTask for book creation and PGN export

--- projects/gui/src/gamedatabasedlg.cpp
+++ projects/gui/src/gamedatabasedlg.cpp
@@ -37,7 +37,7 @@
 #include "gameviewer.h"
 #include "pgndatabase.h"
 #include "gamedatabasesearchdlg.h"
-
+#include "threadedtask.h"
 
 class PgnGameIterator
 {
@@ -45,6 +45,7 @@ class PgnGameIterator
 		PgnGameIterator(GameDatabaseDialog* dlg);
 		~PgnGameIterator();
 
+		int count() const;
 		bool hasNext() const;
 		PgnGame next(bool* ok, int depth = INT_MAX - 1);
 
@@ -69,6 +70,11 @@ PgnGameIterator::~PgnGameIterator()
 {
 	if (m_db != 0)
 		m_db->closeFile();
+}
+
+int PgnGameIterator::count() const
+{
+	return m_gameCount;
 }
 
 bool PgnGameIterator::hasNext() const
@@ -96,6 +102,136 @@ PgnGame PgnGameIterator::next(bool* ok, int depth)
 	*ok = (m_db->game(entry, &game, depth, true) == PgnDatabase::NoError);
 
 	return game;
+}
+
+
+class BookExportTask : public ThreadedTask
+{
+	Q_OBJECT
+
+	public:
+		BookExportTask(PgnGameIterator* it,
+			       QDataStream* out,
+			       int maxDepth,
+			       QWidget* parent);
+		virtual ~BookExportTask();
+
+	protected:
+		virtual void run();
+
+	private:
+		PgnGameIterator* m_it;
+		QDataStream* m_out;
+		int m_depth;
+};
+
+BookExportTask::BookExportTask(PgnGameIterator* it,
+			       QDataStream* out,
+			       int maxDepth,
+			       QWidget* parent)
+	: ThreadedTask(tr("Export Opening Book"),
+		       tr("Parsing %1 PGN games...").arg(it->count()),
+		       0, it->count(),
+		       parent),
+	  m_it(it),
+	  m_out(out),
+	  m_depth(maxDepth)
+{
+}
+
+BookExportTask::~BookExportTask()
+{
+	delete m_out->device();
+	delete m_out;
+	delete m_it;
+}
+
+void BookExportTask::run()
+{
+	PolyglotBook openingBook;
+
+	int i = 0;
+	while (m_it->hasNext())
+	{
+		bool ok;
+		PgnGame game(m_it->next(&ok, m_depth));
+
+		if (ok)
+		{
+			openingBook.import(game, m_depth);
+			if (++i % 512 == 0)
+			{
+				if (cancelRequested())
+					return;
+				emit progressValueChanged(i);
+			}
+		}
+	}
+
+	emit statusMessageChanged(tr("Writing opening book to disk..."));
+	*m_out << &openingBook;
+	emit progressValueChanged(i);
+}
+
+
+class PgnExportTask : public ThreadedTask
+{
+	Q_OBJECT
+
+	public:
+		PgnExportTask(PgnGameIterator* it,
+			      QTextStream* out,
+			      QWidget* parent);
+		virtual ~PgnExportTask();
+
+	protected:
+		virtual void run();
+
+	private:
+		PgnGameIterator* m_it;
+		QTextStream* m_out;
+};
+
+PgnExportTask::PgnExportTask(PgnGameIterator* it,
+			     QTextStream* out,
+			     QWidget* parent)
+	: ThreadedTask(tr("Export Games"),
+		       tr("Writing %1 games to file...").arg(it->count()),
+		       0, it->count(),
+		       parent),
+	  m_it(it),
+	  m_out(out)
+{
+}
+
+PgnExportTask::~PgnExportTask()
+{
+	delete m_out->device();
+	delete m_out;
+	delete m_it;
+}
+
+void PgnExportTask::run()
+{
+	int i = 0;
+	while (m_it->hasNext())
+	{
+		bool ok;
+		PgnGame game(m_it->next(&ok));
+
+		if (ok)
+		{
+			*m_out << game;
+			if (++i % 512 == 0)
+			{
+				if (cancelRequested())
+					return;
+				emit progressValueChanged(i);
+			}
+		}
+	}
+
+	emit progressValueChanged(i);
 }
 
 
@@ -332,67 +468,56 @@ void GameDatabaseDialog::exportPgn()
 	if (fileName.isEmpty())
 		return;
 
-	QFile file(fileName);
-	if (!file.open(QIODevice::WriteOnly))
+	QFile* file = new QFile(fileName);
+	if (!file->open(QIODevice::WriteOnly))
 	{
 		QMessageBox::critical(this, tr("File Error"),
 				      tr("Error while saving file %1\n%2")
-				      .arg(fileName).arg(file.errorString()));
+				      .arg(fileName).arg(file->errorString()));
+		delete file;
 		return;
 	}
-	QTextStream out(&file);
+	QTextStream* out = new QTextStream(file);
 
-	PgnGameIterator it(this);
-	while (it.hasNext())
-	{
-		bool ok;
-		PgnGame game(it.next(&ok));
-
-		if (ok)
-			out << game;
-	}
+	PgnExportTask* task = new PgnExportTask(new PgnGameIterator(this),
+						out, this);
+	task->start();
 }
 
 void GameDatabaseDialog::createOpeningBook()
 {
+	bool ok;
+	int depth = QInputDialog::getInt(this, tr("Opening depth"),
+					 tr("Maximum opening depth (plies):"),
+					 20, 1, 1024, 1, &ok);
+	if (!ok)
+		return;
+
 	const QString fileName = QFileDialog::getSaveFileName(this, tr("Create Opening Book"),
 		QString(), tr("Polyglot Book File (*.bin)"));
 
 	if (fileName.isEmpty())
 		return;
 
-	QFile file(fileName);
-	if (!file.open(QIODevice::WriteOnly))
+	QFile* file = new QFile(fileName);
+	if (!file->open(QIODevice::WriteOnly))
 	{
 		QMessageBox::critical(this, tr("File Error"),
 				      tr("Error while saving file %1\n%2")
-				      .arg(fileName).arg(file.errorString()));
+				      .arg(fileName).arg(file->errorString()));
+		delete file;
 		return;
 	}
-	QDataStream out(&file);
+	QDataStream* out = new QDataStream(file);
 
-	bool ok;
-	int depth = QInputDialog::getInt(this, tr("Opening depth"), tr("Maximum opening depth (plies):"),
-		20, 1, 1024, 1, &ok);
-	if (!ok)
-		return;
-
-	PolyglotBook openingBook;
-
-	PgnGameIterator it(this);
-	while (it.hasNext())
-	{
-		bool ok;
-		PgnGame game(it.next(&ok, depth));
-
-		if (ok)
-			openingBook.import(game, depth);
-	}
-
-	out << &openingBook;
+	BookExportTask* task = new BookExportTask(new PgnGameIterator(this),
+						  out, depth, this);
+	task->start();
 }
 
 void GameDatabaseDialog::updateUi()
 {
 	ui->m_createOpeningBookBtn->setEnabled(m_pgnGameEntryModel->rowCount() > 0);
 }
+
+#include "gamedatabasedlg.moc"
