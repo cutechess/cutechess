9b60d6134963a00ca267fe2ef987163242767fa6
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2016-07-22 15:38:47 +0300

    gui: some documentation for SettingsDialog

--- projects/gui/src/settingsdlg.h
+++ projects/gui/src/settingsdlg.h
@@ -1,18 +1,21 @@
 #ifndef SETTINGSDIALOG_H
 #define SETTINGSDIALOG_H
 
-
 #include <QDialog>
 
 namespace Ui {
 	class SettingsDialog;
 }
 
+/*!
+ * \brief Dialog for configuring global settings
+ */
 class SettingsDialog : public QDialog
 {
 	Q_OBJECT
 
 	public:
+		/*! Creates a new SettingsDialog. */
 		SettingsDialog(QWidget* parent = nullptr);
 		/*! Destroys the dialog. */
 		virtual ~SettingsDialog();
