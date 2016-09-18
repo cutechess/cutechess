df7dbef48d549fd7b467e46b28224bb8b08db126
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2016-09-18 15:35:11 +0300

    lib: set "Termination" tag if game ends with illegal move (fixes #131)

--- projects/lib/src/pgngame.cpp
+++ projects/lib/src/pgngame.cpp
@@ -451,6 +451,9 @@ void PgnGame::setResult(const Chess::Result& result)
 	case Chess::Result::Disconnection:
 		setTag("Termination", "abandoned");
 		break;
+	case Chess::Result::IllegalMove:
+		setTag("Termination", "illegal move");
+		break;
 	case Chess::Result::NoResult:
 		setTag("Termination", "unterminated");
 		break;
