acd614a0f1c21a7d68ccc5fa9ea4365f0e1f7e11
alwey <alwey@free.software>
2017-07-07 02:18:40 +0200

    support Embassy Chess

---
+++ projects/lib/src/board/embassyboard.cpp
@@ -0,0 +1,49 @@
+/*
+    This file is part of Cute Chess.
+
+    Cute Chess is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cute Chess is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cute Chess.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "embassyboard.h"
+
+
+namespace Chess {
+
+EmbassyBoard::EmbassyBoard()
+	: CapablancaBoard()
+{
+}
+
+Board* EmbassyBoard::copy() const
+{
+	return new EmbassyBoard(*this);
+}
+
+QString EmbassyBoard::variant() const
+{
+	return "embassy";
+}
+
+QString EmbassyBoard::defaultFenString() const
+{
+	return "rnbqkcabnr/pppppppppp/10/10/10/10/PPPPPPPPPP/RNBQKCABNR w KQkq - 0 1";
+}
+
+int EmbassyBoard::castlingFile(WesternBoard::CastlingSide castlingSide) const
+{
+	Q_ASSERT(castlingSide != NoCastlingSide);
+	return castlingSide == QueenSide ? 1 : 7; // b-file and h-file
+}
+
+} // namespace Chess
---
+++ projects/lib/src/board/embassyboard.h
@@ -0,0 +1,52 @@
+/*
+    This file is part of Cute Chess.
+
+    Cute Chess is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cute Chess is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cute Chess.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef EMBASSYBOARD_H
+#define EMBASSYBOARD_H
+
+#include "capablancaboard.h"
+
+namespace Chess {
+
+/*!
+ * \brief A board for Embassy chess
+ *
+ * Embassy chess is a variant of Capablanca chess that uses a
+ * different starting position. It is similar to the line-up
+ * of Grand Chess. The king goes to the b- or i-file when castling.
+ *
+ * This variant was introduced in 2005 by Kevin Hill.
+ *
+ * \note Rules: https://en.wikipedia.org/wiki/Embassy_Chess
+ * \sa CapablancaBoard
+ * \sa GothicBoard
+ */
+class LIB_EXPORT EmbassyBoard : public CapablancaBoard
+{
+	public:
+		/*! Creates a new EmbassyBoard object. */
+		EmbassyBoard();
+
+		// Inherited from CapablancaBoard
+		virtual Board* copy() const;
+		virtual QString variant() const;
+		virtual QString defaultFenString() const;
+		virtual int castlingFile(CastlingSide castlingSide) const;
+};
+
+} // namespace Chess
+#endif // EMBASSYBOARD_H
