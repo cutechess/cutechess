c0976e1243364d50311caeacc63a38c356eb4618
alwey <alwey@free.software>
2017-07-21 22:58:17 +0200

    support Andernach chess

---
+++ projects/lib/src/board/andernachboard.cpp
@@ -0,0 +1,142 @@
+/*
+    This file is part of Cute Chess.
+
+    Cute Chess is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cute Chess is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cute Chess.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "andernachboard.h"
+
+namespace Chess {
+
+AndernachBoard::AndernachBoard()
+	: StandardBoard()
+{
+}
+
+Board* AndernachBoard::copy() const
+{
+	return new AndernachBoard(*this);
+}
+
+QString AndernachBoard::variant() const
+{
+	return "andernach";
+}
+
+Move AndernachBoard::moveFromSanString(const QString& str)
+{
+	// import: ignore redundant move information in brackets: Nxd5(=bN)
+	const QString istr = str.left(str.indexOf("("));
+	return WesternBoard::moveFromSanString(istr);
+}
+
+/*
+ * Cutechess follows convention and writes redundant information about side
+ * changes to SAN: e.g. Nxd5(=bN) instead of Nxd5. Imports both formats.
+ */
+QString AndernachBoard::sanMoveString(const Move& move)
+{
+	Piece pc = pieceAt(move.sourceSquare());
+	if (!switchesSides(move))
+		return WesternBoard::sanMoveString(move);
+
+	//append new piece color and type in brackets
+	int promotion = move.promotion();
+	int type = (promotion == Piece::NoPiece) ? pc.type() : promotion;
+	Piece piece(pc.side(), type);
+	QString color = (piece.side() == Side::White) ? "b" : "w";
+	QString a = "(=" + color + pieceSymbol(piece).toUpper() + ")";
+
+	return WesternBoard::sanMoveString(move) + a;
+}
+
+void AndernachBoard::vMakeMove(const Move& move,
+			       BoardTransition* transition)
+{
+	bool moveSwitchesSides = switchesSides(move);
+	StandardBoard::vMakeMove(move, transition);
+
+	// switch side if the move is a capture (not by king)
+	Piece piece = pieceAt(move.targetSquare());
+	if (piece.isValid()
+	&&  moveSwitchesSides)
+		piece.setSide(piece.side().opposite());
+
+	setSquare(move.targetSquare(), piece);
+}
+
+void AndernachBoard::vUndoMove(const Move& move)
+{
+	// switch side on target square if occupied by opposite side
+	Piece piece = pieceAt(move.targetSquare());
+	if (piece.side() == sideToMove().opposite())
+		piece.setSide(piece.side().opposite());
+
+	setSquare(move.targetSquare(), piece);
+
+	StandardBoard::vUndoMove(move);
+}
+
+bool AndernachBoard::switchesSides(const Move& move) const
+{
+	return captureType(move) != Piece::NoPiece
+	&&     pieceAt(move.sourceSquare()).type() != King;
+}
+
+
+
+AntiAndernachBoard::AntiAndernachBoard()
+	: AndernachBoard()
+{
+}
+
+Board* AntiAndernachBoard::copy() const
+{
+	return new AntiAndernachBoard(*this);
+}
+
+QString AntiAndernachBoard::variant() const
+{
+	return "antiandernach";
+}
+
+bool AntiAndernachBoard::switchesSides(const Move& move) const
+{
+	return captureType(move) == Piece::NoPiece
+	&&     pieceAt(move.sourceSquare()).type() != King;
+}
+
+
+
+SuperAndernachBoard::SuperAndernachBoard()
+	: AndernachBoard()
+{
+}
+
+Board* SuperAndernachBoard::copy() const
+{
+	return new SuperAndernachBoard(*this);
+}
+
+QString SuperAndernachBoard::variant() const
+{
+	return "superandernach";
+}
+
+bool SuperAndernachBoard::switchesSides(const Move& move) const
+{
+	return pieceAt(move.sourceSquare()).type() != King;
+}
+
+} // namespace Chess
---
+++ projects/lib/src/board/andernachboard.h
@@ -0,0 +1,122 @@
+/*
+    This file is part of Cute Chess.
+
+    Cute Chess is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cute Chess is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cute Chess.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ANDERNACHBOARD_H
+#define ANDERNACHBOARD_H
+
+#include "standardboard.h"
+
+namespace Chess {
+
+/*!
+ * \brief A board for Andernach Chess
+ *
+ * Andernach chess is a variant of standard chess with modified captures.
+ * Capturing pieces - except for kings - change sides. A pawn capturing on
+ * the final rank is promoted first and then changes sides.
+ *
+ * The game was introduced in 1993 during a tournament in Andernach, Germany.
+ * It was suggested by H. P. Rehm and evolved from Double Tibetian Chess.
+ * It is a popular variant in problem chess.
+ *
+ * \note Rules: https://en.wikipedia.org/wiki/Andernach_chess
+ *
+ * AndernachBoard uses Polyglot-compatible zobrist position keys,
+ * so appropriate books in Polyglot format can be used.
+ *
+ * \note Standard Chess Rules: http://www.fide.com/component/handbook/?id=124&view=article
+ * \sa PolyglotBook
+ * \sa AntiAndernachBoard
+ */
+class LIB_EXPORT AndernachBoard : public StandardBoard
+{
+	public:
+		/*! Creates a new AndernachBoard object. */
+		AndernachBoard();
+
+		// Inherited from StandardBoard
+		virtual Board* copy() const;
+		virtual QString variant() const;
+
+	protected:
+		/*!
+		 * Returns true if \a move switches sides of moved piece.
+		 * True for a capture move not by king.
+		 */
+		virtual bool switchesSides(const Move& move) const;
+
+		// Inherited from StandardBoard
+		virtual Move moveFromSanString(const QString& str);
+		virtual QString sanMoveString(const Move& move);
+		virtual void vMakeMove(const Move& move,
+				       BoardTransition* transition);
+		virtual void vUndoMove(const Move &move);
+};
+
+
+/*!
+ * \brief A board for Anti-Andernach Chess
+ *
+ * Anti-Andernach chess is standard chess but non-capturing moves imply a
+ * change of color (except for Kings). Used in problem chess.
+ * \sa AndernachBoard
+ */
+class LIB_EXPORT AntiAndernachBoard : public AndernachBoard
+{
+	public:
+		/*! Creates a new AntiAndernachBoard object. */
+		AntiAndernachBoard();
+
+		// Inherited from AndernachBoard
+		virtual Board* copy() const;
+		virtual QString variant() const;
+
+	protected:
+		/*!
+		 * Returns true if \a move switches sides of moved piece.
+		 * True for a non-capture move not by king.
+		 */
+		virtual bool switchesSides(const Move& move) const;
+};
+
+/*!
+ * \brief A board for Super-Andernach Chess
+ *
+ * All moves except for King moves imply a change of color in
+ * Super-Andernach chess. This variant is used in problem chess.
+ * \sa AndernachBoard
+ */
+class LIB_EXPORT SuperAndernachBoard : public AndernachBoard
+{
+	public:
+		/*! Creates a new SuperAndernachBoard object. */
+		SuperAndernachBoard();
+
+		// Inherited from AndernachBoard
+		virtual Board* copy() const;
+		virtual QString variant() const;
+
+	protected:
+		/*!
+		 * Returns true if \a move switches sides of moved piece.
+		 * True for any move not by king.
+		 */
+		virtual bool switchesSides(const Move& move) const;
+};
+
+} // namespace Chess
+#endif // ANDERNACHBOARD_H
