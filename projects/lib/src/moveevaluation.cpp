fb4c5797676d3e17948e8eae2a3c0be75028dfdd
Ilari Pihlajisto <ilari.pihlajisto@vionice.fi>
2017-08-30 09:49:52 +0300

    fix extremely high scores in EvalWidget (issue #274)

--- projects/lib/src/moveevaluation.cpp
+++ projects/lib/src/moveevaluation.cpp
@@ -109,27 +109,26 @@ QString MoveEvaluation::scoreText() const
 {
 	if (isBookEval())
 		return "book";
-	if (isEmpty())
+	if (m_score == NULL_SCORE)
 		return QString();
 
 	QString str;
 	if (depth() > 0)
 	{
-		int scoreVal = score();
-		int absScore = qAbs(scoreVal);
-		if (scoreVal > 0)
+		int absScore = qAbs(m_score);
+		if (m_score > 0)
 			str += "+";
 
 		// Detect mate-in-n scores
 		if (absScore > 98800
 		&&  (absScore = 1000 - (absScore % 1000)) < 200)
 		{
-			if (scoreVal < 0)
+			if (m_score < 0)
 				str += "-";
 			str += "M" + QString::number(absScore);
 		}
 		else
-			str += QString::number(double(scoreVal) / 100.0, 'f', 2);
+			str += QString::number(double(m_score) / 100.0, 'f', 2);
 	}
 
 	return str;
