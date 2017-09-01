c37997802cd9139d18764bf6164b05abf74b9056
alwey <alwey@free.software>
2017-09-01 18:13:40 +0200

    mark duplicates in engine configuration list

--- projects/gui/src/engineconfigurationmodel.cpp
+++ projects/gui/src/engineconfigurationmodel.cpp
@@ -102,6 +102,13 @@ QVariant EngineConfigurationModel::data(const QModelIndex& index, int role) cons
 		if (!engine.supportsVariant(m_chessVariant))
 			return QBrush(Qt::red);
 
+		int count = 0;
+		const auto& engines = m_engineManager->engines();
+		for (const EngineConfiguration& engine2: engines)
+			if (engine.name() == engine2.name())
+				if (++count > 1)
+					return QBrush(Qt::gray);
+
 		return QVariant();
 	}
 
