4cc4e5d6e8e876b565397acba13fa506a22bf6ae
alwey <alwey@free.software>
2020-10-06 23:56:57 +0200

    Add result text for maximal game length adjudication

--- projects/lib/src/gameadjudicator.cpp
+++ projects/lib/src/gameadjudicator.cpp
@@ -146,7 +146,7 @@ void GameAdjudicator::addEval(const Chess::Board* board, const MoveEvaluation& e
 	if (m_maxGameLength > 0
 	&&  board->plyCount() >= 2 * m_maxGameLength)
 	{
-		m_result = Chess::Result(Chess::Result::Adjudication, Chess::Side::NoSide);
+		m_result = Chess::Result(Chess::Result::Adjudication, Chess::Side::NoSide, "maximal game length");
 		return;
 	}
 }
